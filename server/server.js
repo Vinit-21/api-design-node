b91ef20bad00e41fddac8d1dc761585289cfc06a
Glitch (materialistic-strawflower) <none>
2019-12-29 06:51:52 +0000

    👳🏐 Updated with Glitch

--- server/server.js
+++ server/server.js
@@ -1,80 +1,83 @@
-// TODO: make this work.
-// if yuo go to localhost:3000 the app
-// there is expected crud to be working here
+// TODO: user app.params to find the lion using the id
+// and then attach the lion to the req object and call next. Then in
+// '/lion/:id' just send back req.lion
+
+// create a middleware function to catch and handle errors, register it
+// as the last middleware on app
+
+// create a route middleware for POST /lions that will increment and
+// add an id to the incoming new lion object on req.body
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
-
-// express.static will serve everything
-// with in client as a static resource
-// also, it will server the index.html on the
-// root of that directory on a GET to '/'
-app.use(express.static('client'));
-
-// body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
+var morgan = require('morgan');
 
 var lions = [];
 var id = 0;
 
-// TODO: make the REST routes to perform CRUD on lions
+var updateId = function(req, res, next) {
+  // fill this out. this is the route middleware for the ids
+  if(req.body.name != undefined){
+    id+=1;
+    req.body.id = id+'';
+  }
+  next();
+};
 
-app.get('/lions',(req,res) => {
+app.use(morgan('dev'))
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.param('id', function(req, res, next, id) {
+  // fill this out to find the lion based off the id
+  // and attach it to req.lion. Rember to call next()
+  var lionIndex = _.findIndex(lions, {id: id});
+  if(lionIndex == -1){
+    res.send();
+  }
+  else{
+    req.lion = lions[lionIndex];
+    next();
+  }
+});
+
+app.get('/lions', function(req, res){
   res.json(lions);
 });
 
-app.get('/lions/:id',(req,res) => {
-  var lionIndex = _.findIndex(lions, {id: req.params.id});
-  if(lionIndex == -1){
-    res.json({message: "Lion not Found"}); 
-  }
-  else{
-    res.json(lions[lionIndex]);
-  }
+app.get('/lions/:id', function(req, res){
+  // use req.lion
+  var lion = req.lion;
+  res.json(lion || {});
 });
 
-app.post('/lions',(req,res) => {
+app.post('/lions', updateId, function(req, res) {
   var lion = req.body;
-  if(lion.name != undefined){
-  lion.id = ''+(id+1);
-  id+=1
+
   lions.push(lion);
+
   res.json(lion);
+});
+
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
   }
-  else{
-    res.json({message: "Invalid post"});
+
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
   }
 });
 
-app.put('/lions/:id',(req,res) => {
-  var lionIndex = _.findIndex(lions, {id: req.params.id});
-  if(lionIndex == -1){
-    res.json({message: "Lion not Found"});
-  }
-  else{
-    var lionId = lions[lionIndex].id;
-    lions[lionIndex] = req.body;
-    lions[lionIndex].id = lionId;
-    res.json(lions[lionIndex]);
-  }
-});
-
-app.delete('/lions/:id',(req,res) => {
-  var lionIndex = _.findIndex(lions, {id: req.params.id});
-  if(lionIndex == -1){
-    res.json({message: "Lion not Found"});
-  }
-  else{
-    var lion = lions[lionIndex];
-    lions.splice(lionIndex,1);
-    res.json(lion);
-  }
-});
-
-app.listen(3000,function(){
+app.listen(3000);
 console.log('on port 3000');
-});
